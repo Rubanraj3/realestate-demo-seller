19e06eaffdd641aa39621f4b14e9fe8872fe2fda
Rexanruban <rexansrls@gmail.com>
2023-12-12 15:12:24 +0530

    'd'

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -4,8 +4,8 @@ export const Env = {
   // baseAPi: "https://kapture.click",
   // baseAPi: "https://wayr.link",
   // baseAPi: "https://www.lushgreen.click",
-  baseAPi: "http://localhost:3001",
-  // baseAPi: "https://uyarchicrm.click"
+  // baseAPi: "http://localhost:3001",
+  baseAPi: "https://uyarchicrm.click"
 
 }
 
